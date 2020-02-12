d6e500b4ce4a6446f76ac32c6cdb20e5f9bb0a7d
maxicraftone <trashmco@gmail.com>
2020-02-12 23:09:26 +0100

    Renamed ascii_bot to translator_bot.

---
+++ translator_bot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import discord
+
+class Translator_Client(discord.Client):
+    prefix = '$'
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, msg):
+        if msg.author != self.user:
+            if msg.content.startswith(self.prefix):
+                command = msg.content[1:].split(' ')[0]
+                args = msg.content[1:].split(' ')[1:]
+                if command == 'asciify':
+                    await self.asciify(args)
+
+
+    async def asciify(self, *args):
+        print(args)
+
+client = Translator_Client()
+client.run('Njc3MTA3NjA1ODQzMjE0MzM2.XkPbsQ.9zQHm4B8kJdtJf9Ga74JHknQ__8')
