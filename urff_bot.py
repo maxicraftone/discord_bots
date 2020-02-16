dc1643646371231e3a29afde163d67713059beb9
maxicraftone <trashmco@gmail.com>
2020-02-16 13:42:52 +0100

    'URFF' now in caps

--- urff_bot.py
+++ urff_bot.py
@@ -3,9 +3,8 @@ from bot_class import Bot
 
 class UrffBot(Bot):
     async def on_message(self, msg):
-        if 'uff' in msg.content.lower() ||
-           'oof' in msg.content.lower():
-            await msg.channel.send('urff')
+        if 'uff' in msg.content.lower() or 'oof' in msg.content.lower():
+            await msg.channel.send('URFF')
 
 if __name__ == '__main__':
     urff_bot = UrffBot('urff_bot')
