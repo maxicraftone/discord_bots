cc8bc76167c5fda6ba5fd0c95371344450795bf5
maxicraftone <trashmco@gmail.com>
2020-03-05 23:11:07 +0100

    Fixed errors

--- example_bot.py
+++ example_bot.py
@@ -7,5 +7,5 @@ class ExampleBot(Bot):
         await msg.channel.send('Hiya')
 
 if __name__ == '__main__':
-    example_bot = TranslatorBot('example_bot', '$')
+    example_bot = ExampleBot('example_bot', '$')
     example_bot.run(example_bot.get_token())
